2012b4e5ccff879f82bf33336ead91a76c946b6a
paolometeo <paolometeo@users.noreply.github.com>
2021-03-15 12:09:06 +0100

    Delete KIT directory

--- KIT/DHT22_TX/Battery.ino
+++
@@ -1,7 +0,0 @@
-void Battery(){
-//  Battery Power  Legge la tensione della pila e la converte in mV interi
-word outanalog = analogRead(VBATPIN);
-//Serial.println(outanalog);
-float measuredvbat = 3300.0 * 2 * float(outanalog)/1023.0;
-vBatTX = Nword(measuredvbat);
-}
--- KIT/DHT22_TX/SensInit.ino
+++
@@ -1,6 +0,0 @@
-//  put here instructions for initializing sensors and values for ID_TX and Length
-void SensInit(){
-     // Call dht.begin() to initialize the sensor and our data pin
-  dht.begin();
-  altDelay(1000);
-}
